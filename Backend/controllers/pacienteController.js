51aee9f5114fc406165fbf1cd60745780c3c6330
JavierBalcazarCruz <javierbalcazar80@gmail.com>
2024-02-17 16:41:57 -0600

    se añadio la funcionalidad para actualizar a un paciente

--- Backend/controllers/pacienteController.js
+++ Backend/controllers/pacienteController.js
@@ -24,6 +24,10 @@ const obtenerPacientes = async (req, res) => {
 const obtenerPaciente = async (req, res) => {
     const {id} = req.params;
     const paciente = await Paciente.findById(id);
+    if(!paciente)
+    {
+       return res.status(404).json({msg:'Paciente no encontrado'});
+    }
     //Validar que ese paciente fue agregado por el doctor que esta autenticado, solo el puede verlo
     //Si el medico que inicio sesion es diferente de la busqueda de el veterinario validado por el token entonces no tiene permisos
     if(paciente.veterinario._id.toString() !== req.veterinario._id.toString())
@@ -34,10 +38,40 @@ const obtenerPaciente = async (req, res) => {
     {
         return res.json(paciente);
     }
+  
 }
 
 const actualizarPaciente = async (req, res) => {
     //Se va pasar el id de cada paciente
+        const {id} = req.params;
+        const paciente = await Paciente.findById(id);
+        //Validar que ese paciente fue agregado por el doctor que esta autenticado, solo el puede verlo
+        //Si el medico que inicio sesion es diferente de la busqueda de el veterinario validado por el token entonces no tiene permisos
+        if(!paciente)
+        {
+           return res.status(404).json({msg:'Paciente no encontrado'});
+        }
+
+        if(paciente.veterinario._id.toString() !== req.veterinario._id.toString())
+        {
+            return res.json({msg: 'No tienes permiso para acceder a este paciente'});
+        }
+        if(paciente)
+        {
+            //Actualizar paciente
+            //si esta precente req.body.nombre o si no ponle paciente.nombre
+            paciente.nombre = req.body.nombre || paciente.nombre;
+            paciente.propietario = req.body.propietario || paciente.propietario;
+            paciente.email = req.body.email || paciente.email;
+            paciente.fecha = req.body.fecha || paciente.fecha;
+            paciente.sintomas = req.body.sintomas || paciente.sintomas;
+            try {
+                const pacienteActualizado = await paciente.save();
+                res.json(pacienteActualizado);
+            } catch (error) {
+                console.log(error);
+            }
+        }    
 };
 
 const eliminarPaciente = async (req, res) => {
