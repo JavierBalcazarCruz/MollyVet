e2ceab4ae4c1769dfbcea72addd75345140aadb7
JavierBalcazarCruz <javierbalcazar80@gmail.com>
2024-02-25 20:08:33 -0600

    Se modifico el login ya que contenia el login, el registrarse, se comenso a dividir

--- Frontend/src/paginas/Registrar.jsx
+++ Frontend/src/paginas/Registrar.jsx
@@ -1,8 +1,42 @@
 
+import {Link} from 'react-router-dom';
+import { useEffect,useState  } from 'react';
+import '../assets/registrar/styles/style.css'; // Importa los estilos CSS
+import logImg from '../assets/login/images/log.png';
+import regImg from '../assets/login/images/log.png';
 
 const Registrar = () => {
+  const [signUpMode, setSignUpMode] = useState(false);
+  useEffect(() => {
+    setSignUpMode(true);
+}, [])
   return (
-    <div>Registrar</div>
+    <div className={`container ${signUpMode ? 'sign-up-mode' : ''}`}>
+   <div className="panel right-panel">
+    <div className="content">
+      <h3>¿Ya tienes cuenta?</h3>
+      <p>
+          Inicia sesión y comienza a administrar tus veterinaria
+      </p>
+      <button className="btn transparent" id="sign-in-btn">
+       Iniciar sesión
+      </button>
+    </div>
+    <img src={logImg} className="image" alt="" />        
+  </div>
+
+  </div>
+
+
+
+    
+
+
+
+
+
+
+   
   )
 }
 
